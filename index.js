708e6de1fe786e527e977101b17b60bee8224757
BuildTools <unconfigured@null.spigotmc.org>
2022-04-20 11:06:36 +0200

    added models

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 require("dotenv").config();
 
-mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true }, () => {
+mongoose.connect("mongodb+srv://fullstack:project@mydatabase.4knzk.mongodb.net/FullstackProject?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true }, () => {
     signale.success("Connected to database");
 });
 
