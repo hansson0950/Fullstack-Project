abcde6150d8e548d91da592616ee7cbacf98ae09
Simon-W1 <1simon.wikstrom@gmail.com>
2022-04-20 13:28:40 +0200

    one more requirement

--- validation.js
+++ validation.js
@@ -3,7 +3,7 @@ const Joi = require("@hapi/joi");
 const registerValidation = data => {
     const schema = Joi.object({
         name: Joi.string().min(2).max(20).required(),
-        email: Joi.string().required().email(),
+        email: Joi.string().max(50).required().email(),
         password: Joi.string().min(6).max(20).required()
     });
     return schema.validate(data);
