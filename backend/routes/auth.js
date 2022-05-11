b7ea4b8bfdded200b6c8552b15a021e794ca38ca
BuildTools <unconfigured@null.spigotmc.org>
2022-05-11 15:51:38 +0200

    changed login redirect

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -25,7 +25,7 @@ router.post("/register", async (req, res) => {
     try {
         const savedUser = await user.save();
         const token = jwt.sign({ _id: user._id }, process.env.TOKEN);
-        res.json({ user: user._id, redirect: "../home", token });
+        res.json({ user: user._id, redirect: "../order", token });
     } catch (err) {
         res.status(400).json(err);
     }
@@ -36,15 +36,13 @@ router.post("/login", async (req, res) => {
     if (error) return res.status(400).json({ error: error.details[0].message });
 
     const user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).json({ error: "Email is not found" });
+    if (!user) return res.status(400).json({ error: "Email not found" });
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).json({ error: "Invalid Password" });
 
     const token = jwt.sign({ _id: user._id }, process.env.TOKEN);
-    res.header("auth-token", token).json({ token, redirect: "../home" });
-
-    signale.complete("Login Successful")
+    res.header("auth-token", token).json({ token, redirect: "../order" });
 });
 
 module.exports = router;
