d19125f9ee67b0491610a5aabb30a31c1719d29d
hansson0950 <82034963+hansson0950@users.noreply.github.com>
2022-05-16 12:26:26 +0200

    Delete frontend/about directory

--- frontend/about/script.js
+++
@@ -1,8 +0,0 @@
-// Script to make the menu come down and up (hamburger)
-
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
-});
